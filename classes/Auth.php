59c6b37a70086ffff2ce755d3c2173c74306aa7b
Chinoms <chinoms925@gmail.com>
2019-10-19 16:18:13 +0100

    Update Signup
    Simplified the signup process.
    Allow users to sign up.
    Allow vendors to sign up as vendors.

--- classes/Auth.php
+++ classes/Auth.php
@@ -1,9 +1,43 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once("config.php");
 
 class auth
 {
-    public function signupVenodor($conn, $email, $phoneNum, $fname, $lname, $businessname, $cansell, $displaymode, $pass1, $pass2, $vendorurl)
+    public function signup($conn, $email, $phoneNum, $fname, $lname, $pass1, $pass2)
+    {
+        $conn->query("SELECT * FROM users WHERE us_email = '$email' OR us_phone = '$phoneNum'");
+        if ($conn->affected_rows > 0) { //check if user exists
+            $userExists['code'] = 419;
+            $userExists['msg'] = "UserExists";
+            header('content-type:application/json');
+            echo json_encode($userExists);
+            die();
+        } else {
+            if ($pass1 == $pass2) { //check if passwords match
+                $password = password_hash($pass1, PASSWORD_DEFAULT); //hash password
+                $newUser = $conn->prepare("INSERT INTO users(us_email, us_phone, us_password, us_fname, us_lname) VALUES(?, ?, ?, ?, ?)");
+                $newUser->bind_param("sssss", $email, $phoneNum, $password, $fname, $lname);
+                if ($newUser->execute()) {
+                    $success['code'] = 200;
+                    $success['msg'] = "usercreated";
+                    header('content-type:application/json');
+                    echo json_encode($success);
+                }
+            } else {
+                $passwordMismatch['code'] = 419;
+                $passwordMismatch['msg'] = "passwordmismatch";
+                header('content-type:application/json');
+                echo json_encode($passwordMismatch);
+            }
+        }
+    }
+
+
+
+
+    public function signupVendor($conn, $email, $phoneNum, $fname, $lname, $businessname, $cansell, $displaymode, $pass1, $pass2, $vendorurl)
     {
         $conn->query("SELECT * FROM users WHERE us_email = '$email' OR us_phone = '$phoneNum'");
         if ($conn->affected_rows > 0) { //check if user exists
@@ -41,6 +75,8 @@ class auth
             }
         }
     }
+
+
 
     public function loginUser($conn, $phoneNum, $pass)
     {
--- modules/signup.php
+++ modules/signup.php
@@ -2,24 +2,35 @@
 require_once("../classes/Auth.php");
 
 if (isset($_REQUEST['token']) && $_REQUEST['token'] == hash("sha256", "easymarket..")) {
-//$email, $phoneNum, $password, $fname, $lname, $businessname, $cansell, $displaymode, $pass1, $pass2, $vendorurl
-$email = $_REQUEST['email'];
-$phoneNum = $_REQUEST['phonenum'];
-$fname = $_REQUEST['fname'];
-$lname = $_REQUEST['lname'];
-$businessname = $_REQUEST['bizname'];
-$cansell = $_REQUEST['cansell'];
-$displaymode = $_REQUEST['displaymode'];
-$pass1 = $_REQUEST['pass1'];
-$pass2 = $_REQUEST['pass2'];
-$vendorurl = $_REQUEST['vendorurl'];
+    //$email, $phoneNum, $password, $fname, $lname, $businessname, $cansell, $displaymode, $pass1, $pass2, $vendorurl
+    $email = $_REQUEST['email'];
+    $phoneNum = $_REQUEST['phonenum'];
+    $fname = $_REQUEST['fname'];
+    $lname = $_REQUEST['lname'];
+    if (isset($_REQUEST['bizname'])) {
+        $businessname = $_REQUEST['bizname'];
+    }
+    if (isset($_REQUEST['cansell'])) {
+        $cansell = $_REQUEST['cansell'];
+    }
+    if (isset($_REQUEST['displaymode'])) {
+        $displaymode = $_REQUEST['displaymode'];
+    }
+    $pass1 = $_REQUEST['pass1'];
+    $pass2 = $_REQUEST['pass2'];
+    if (isset($_REQUEST['vendorurl'])) {
+        $vendorurl = $_REQUEST['vendorurl'];
+    }
 
 
-$authClass-> signupVenodor($conn, $email, $phoneNum, $fname, $lname, $businessname, $cansell, $displaymode, $pass1, $pass2, $vendorurl);
-}  else {
+    if (isset($_REQUEST['cansell'])) {
+        $authClass->signupVendor($conn, $email, $phoneNum, $fname, $lname, $businessname, $cansell, $displaymode, $pass1, $pass2, $vendorurl);
+    } else {
+        $authClass->signup($conn, $email, $phoneNum, $fname, $lname, $pass1, $pass2);
+    }
+} else {
     $error['code'] = 400;
     $error['msg'] = "bad_token";
     header('content-type:application/json');
     echo json_encode($error);
 }
-?>
